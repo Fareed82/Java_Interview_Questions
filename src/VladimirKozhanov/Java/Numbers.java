139fe09ea89c77c110d946b7e872e438fbd10430
morb1k <morbik.morbik@gmail.com>
2023-05-22 16:03:28 -0400

    Updated Numbers.java

--- src/VladimirKozhanov/Java/Numbers.java
+++ src/VladimirKozhanov/Java/Numbers.java
@@ -8,21 +8,17 @@ public class Numbers {
         for (int i = 0; i < 5; i++) {
             System.out.println("Gimme a first number (dividend): ");
             int num1 = scan.nextInt();
-
             System.out.println("Gimme a second number (divisor): ");
             int num2 = scan.nextInt();
-
             divide(num1, num2);
         }
-
     }
-
     //Well ill update it later. Maybe.
 
     public static void divide(int num1, int num2) {
         int count = 0;
         int neg = 1;
-        if (!isPostiveResult(num1, num2)) {
+        if (!(num1 > 0 && num2 > 0 || num1 < 0 && num2 < 0)) {
             neg = -1;
             if (num1 < 0) {
                 num1 *= -1;
@@ -35,13 +31,6 @@ public class Numbers {
             num1 -= num2;
             count++;
         }
-
         System.out.println(count * neg);
     }
-
-
-    public static boolean isPostiveResult(int num1, int num2){
-        return num1 > 0 && num2 > 0 || num1 < 0 && num2 < 0;
-    }
-
 }
