19bae85ab54f9f71236eb2b5a1855f56d9583650
morb1k <93850423+morb1k@users.noreply.github.com>
2023-05-22 13:11:54 -0400

    Completed OddOrEven.java

---
+++ src/VladimirKozhanov/Java/OddOrEven.java
@@ -0,0 +1,25 @@
+package VladimirKozhanov.Java;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Gimme a number: ");
+        int num = scan.nextInt();
+
+        identify(num);
+        }
+    }
+
+    public static void identify(int num){
+        if(num % 2 == 0){
+            System.out.println("Even");
+        }else{
+            System.out.println("Odd");
+        }
+    }
+
+}
