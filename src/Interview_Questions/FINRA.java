5b16ad7035e83faef579018c50ae7e095435666a
Fareed_Cydeo <far1882@gmail.com>
2023-05-23 13:03:18 -0500

    Added FINRA task

---
+++ src/Interview_Questions/FINRA.java
@@ -0,0 +1,33 @@
+package Interview_Questions;
+
+public class FINRA {
+
+    public static void main(String[] args) {
+
+        System.out.println("The following will be for numbers from 1 - 30");
+        System.out.println();
+
+        Finra();
+
+    }
+
+    public static void Finra() {
+        String result = "";
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 15 == 0) {
+                result += "FINRA ";
+            } else if (i % 5 == 0) {
+                result += "RA ";
+            } else if (i % 3 == 0) {
+                result += "FIN ";
+            } else {
+                result += i + " ";
+            }
+        }
+        System.out.println(result);
+    }
+
+
+}
