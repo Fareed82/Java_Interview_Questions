879964dd01fe023521cc9b87100472dfff188c22
TatianaRasskazova <trasskazova840@gmail.com>
2023-05-24 13:23:12 -0500

    fixed second Task

--- src/Tati/EvenOrOdd.java
+++ src/Tati/EvenOrOdd.java
@@ -4,13 +4,16 @@ public class EvenOrOdd {
 
     public static String identify(int num) {
 
+        if (num == 0) {
+            return num + " can be divisible by any number";
+        }
         return num % 2 == 0 ? "Even" : "Odd";
 
     }
 
 
     public static void main(String[] args) {
-        System.out.println(identify(5));
+        System.out.println(identify(0));
     }
 
 }
