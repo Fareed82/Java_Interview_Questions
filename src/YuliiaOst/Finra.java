396c319a541723a751fbc4b6ea6fa98e50da5cff
YuliiaOstgit config --global user.name YuliiaOstgit config --global user.name YuliiaOst <yuliya.ostrogrud@gmail.com>
2023-05-24 11:28:59 -0500

    Created Finra

---
+++ src/YuliiaOst/Finra.java
@@ -0,0 +1,36 @@
+package YuliiaOst;
+
+public class Finra {
+
+    public static void printFinra() {
+
+        String result = "";
+
+        for (int i = 1; i <= 30; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                result += "FINRA ";
+
+            } else if (i % 3 == 0) {
+                result += "FIN ";
+
+            } else if (i % 5 == 0) {
+                result += "RA ";
+
+            } else {
+                result += i + " ";
+            }
+        }
+        System.out.println(result);
+    }
+
+    public static void main(String[] args) {
+        printFinra();
+    }
+}
+/*
+Write a method, which prints out the numbers from 1 to 30.
+but for number which is a multiple of 3, print "FIN" instead of the number
+and for number which is a multiple of 5, print "RA" instead of the number.
+and for number which is a multiple of both 3 and 5, print "FINRA" instead of the number.
+ */
