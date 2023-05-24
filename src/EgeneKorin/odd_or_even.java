b63b2004d0a51aeabdcb85eeb43b4b0a30c16443
Eugene <evgen.korin1@gmail.com>
2023-05-24 12:53:40 -0500

    odd or even

---
+++ src/EgeneKorin/odd_or_even.java
@@ -0,0 +1,21 @@
+package EgeneKorin;
+
+public class odd_or_even {
+    public static void main(String[] args) {
+        identify(101);                           // read ours method
+    }
+
+    public static void identify(int number){            // create method
+
+        if(number % 2== 0){                                // if method divided by 2 = 0 then print it is even number
+            System.out.println(number + " is even number");
+        }else{
+            System.out.println(number + " is odd");
+        }
+
+
+
+
+
+    }
+}
