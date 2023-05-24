d3513e8c35456512e1baf8e9e72d56c58d9e93a9
YuliiaOstgit config --global user.name YuliiaOstgit config --global user.name YuliiaOst <yuliya.ostrogrud@gmail.com>
2023-05-24 11:11:01 -0500

    Created OddOrEven class & method

---
+++ src/YuliiaOst/OddOrEven.java
@@ -0,0 +1,36 @@
+package YuliiaOst;
+
+import java.util.Scanner;
+
+public class OddOrEven {
+
+    public static void identifyOddOrEven(int number) {
+
+        if (number % 2 == 0) { // when we divide a number by 2, if a remainder is 0, it means the number is even
+            System.out.println("Number " + number + " is even");
+
+        } else { // if a number is not even, it's odd
+            System.out.println("Number " + number + " is odd");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer number:");
+        int number = input.nextInt();
+
+        identifyOddOrEven(number);
+
+        input.close();
+    }
+}
+
+/*
+Write a method which can identifies given number is even or odd
+
+EX:
+identify(5) -> "Odd"
+identify(6) -> "Even"
+ */
