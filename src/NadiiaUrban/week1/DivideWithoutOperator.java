9b686b2c22e0a5e27ae37dd160338a63ddda8f74
nadiiaurban <nadiiaurban@gmail.com>
2023-05-25 15:57:00 -0600

    added week1 tasks

--- src/NadiiaUrban/week1/DivideWithoutOperator.java
+++ src/NadiiaUrban/week1/DivideWithoutOperator.java
@@ -7,16 +7,17 @@ public class DivideWithoutOperator {
 
     public static void divideNumbers(int num1, int num2){
         int count = 0;
-if(num2==0){
-    throw new ArithmeticException("Divisor  can not be zero");
-}
-else {
 
-        while(num1>num2){
-    num1-=num2;
-    count++;
-}}
-        System.out.println(count+"."+num1%num2);
+    if(num2==0){
+    throw new ArithmeticException("Divisor  can not be zero");
+    }
+
+        while(num1>=num2){
+        num1-=num2;
+        count++;
+       }
+
+        System.out.println(count+"."+num1);
     }
 
 }
