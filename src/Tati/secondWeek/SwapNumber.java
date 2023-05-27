c948bca7d2e77d828a142244977b76cdcb3c2a4c
TatianaRasskazova <trasskazova840@gmail.com>
2023-05-26 21:07:02 -0500

    added second task

--- src/Tati/secondWeek/SwapNumber.java
+++ src/Tati/secondWeek/SwapNumber.java
@@ -2,4 +2,23 @@ package Tati.secondWeek;
 
 public class SwapNumber {
 
+    public static void main(String[] args) {
+
+
+        int x = 6;
+        int y = 4;
+        // y = 6
+        // x = 4
+        x = x + y;
+        y = x - y;
+        x = x - y;
+
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
 }
+/*
+Swap two variables value without using a third variable
+ */
