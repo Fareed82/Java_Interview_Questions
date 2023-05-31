395076acacc5b1209550ed03c770cb8a48bd7e19
wahiba Aabid <aabidwahiba@gmail.com>
2023-05-31 00:20:31 -0700

    Added SwapNumbersWahiba

---
+++ src/SwapNumbersWahiba.java
@@ -0,0 +1,14 @@
+public class SwapNumbersWahiba {
+
+    public static void main(String[] args) {
+
+        int num1 = 5;
+        int num2 = 10;
+     num1 = num1+num2; //15
+     num2 = num1-num2; // 5
+     num1 = num1-num2; // 10
+        System.out.println(num1);
+        System.out.println(num2);
+
+    }
+}
