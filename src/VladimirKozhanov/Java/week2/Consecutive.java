16727777e56c4156769e9b33054c635e7bd0616b
morb1k <93850423+morb1k@users.noreply.github.com>
2023-05-31 15:17:29 -0400

    Updates

--- src/VladimirKozhanov/Java/week2/Consecutive.java
+++ src/VladimirKozhanov/Java/week2/Consecutive.java
@@ -2,6 +2,40 @@ package VladimirKozhanov.Java.week2;
 
 public class Consecutive {
 
+    public static void main(String[] args) {
+int number = 24;
 
+Consecutive(number);
+
+
+
+    }
+
+
+
+    public static void Consecutive(int number){
+
+
+
+        for (int i = 1; i <= number; i++) {
+            String result = "";
+
+
+            if (i % 5 == 0) {
+                result += "Coders";
+            }
+            if (i % 3 == 0) {
+                result += "Test";
+            }
+            if (i % 2 == 0) {
+                result += "Codility";
+            } else {
+                result = "" + i;
+            }
+            System.out.println(result);
+        }
+
+
+    }
 
 }
--- src/VladimirKozhanov/Java/week2/Divisible.java
+++ src/VladimirKozhanov/Java/week2/Divisible.java
@@ -3,22 +3,24 @@ package VladimirKozhanov.Java.week2;
 public class Divisible {
 
     public static void main(String[] args) {
+        String DivBy15 = "";
+        String DivBy3 = "";
+        String DivBy5 = "";
 
-for (int num = 0; num <=100 ; num++) {
-    if (num % 15 == 0 && num % 5 == 0 && num % 3 == 0) {
-        System.out.println("DivisibleBy15");
-    } else if (num % 3 == 0 && num % 15 != 0) {
-        System.out.println("DivisibleBy3");
-    } else if (num % 5 == 0 && num % 15 != 0) {
-        System.out.println("DivisibleBy5");
-    }else{
-        System.out.println(num);
+        for (int num = 0; num < 100; num++) {
+            if (num % 15 == 0 && num % 5 == 0 && num % 3 == 0) {
+                DivBy15 += num + " ";
+            } else if (num % 3 == 0 && num % 15 != 0) {
+                DivBy3 += num + " ";
+            } else if (num % 5 == 0 && num % 15 != 0) {
+                DivBy5 += num + " ";
+            }
+
+        }
+        System.out.println("Divisible By 15 " + DivBy15.trim());
+        System.out.println("Divisible By 3 " + DivBy3.trim());
+        System.out.println("Divisible By 5 " + DivBy5.trim());
+
     }
-}
-
-    }
-
-
-
 
 }
