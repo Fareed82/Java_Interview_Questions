bf57e3a9eb8be0c48038d21d589c135c27be5c91
morb1k <93850423+morb1k@users.noreply.github.com>
2023-06-05 15:10:58 -0400

    Updates

--- src/VladimirKozhanov/Java/week3/ReverseNegative.java
+++ src/VladimirKozhanov/Java/week3/ReverseNegative.java
@@ -1,16 +1,35 @@
 package VladimirKozhanov.Java.week3;
 
+import java.util.Scanner;
+
 public class ReverseNegative {
 
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        System.out.println("Gimme negative number:");
+        int number = scan.nextInt();
 
+        System.out.println(ReverseNumber(number));
 
 
     }
 
 
-    public static int Revers
+    public static int ReverseNumber(int number) {
+        int reversed = 0;
 
+        while (number != 0) {
+            reversed *= 10;
 
+            while (number % 10 != 0) {
+                reversed -= 1;
+                number += 1;
+
+            }
+            number *= 0.1;
+
+        }
+        return reversed;
+    }
 }
