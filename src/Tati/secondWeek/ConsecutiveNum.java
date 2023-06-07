883b4ca49e2f9158d8a0580c06f665dd50da30e8
TatianaRasskazova <trasskazova840@gmail.com>
2023-06-06 22:36:16 -0500

    added Wahiba's decision

--- src/Tati/secondWeek/ConsecutiveNum.java
+++ src/Tati/secondWeek/ConsecutiveNum.java
@@ -11,6 +11,23 @@ public class ConsecutiveNum {
 
             //String result = "";
             for (int i = 1; i <= num; i++) {
+                //WAHIBA
+                /*String result = "";
+                if (i % 2 == 0) {
+                    result += "Codility";
+                }
+                if (i % 3 == 0) {
+                    result += "Test";
+                }
+                if (i % 5 == 0) {
+                    result += "Coders";
+                }
+                if ((i % 5 != 0) && (i % 3 != 0) && (i % 2 != 0)) {
+                    result += i;
+                }
+                System.out.println(result);
+*/
+
                 if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
 
                     System.out.println("CodilityTestCoders");
@@ -39,6 +56,7 @@ public class ConsecutiveNum {
             //System.out.println(result);
         }
     }
+
     public static void main(String[] args) {
         consecutive(-7);
     }
