0a21e674cacc4098233e8812fde188193da0da6f
sayyam <say_wani@yahoo.com>
2023-06-07 12:37:53 -0500

    created division without operator

---
+++ src/Java_Interview_QuestionsWeek3/ReverseNegative.java
@@ -0,0 +1,20 @@
+package Java_Interview_QuestionsWeek3;
+
+public class ReverseNegative {
+
+    public static int reverseNegative(int num) {
+
+        num = Math.abs(num);
+        String str = "" + num;
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
+        }
+        int revnum = Integer.parseInt(reverse);
+        return revnum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseNegative(-69));
+    }
+}
---
+++ src/javaInterviewQuestions/DivisionWithoutOperators.java
@@ -0,0 +1,50 @@
+package javaInterviewQuestions;
+
+import java.util.Scanner;
+
+public class DivisionWithoutOperators {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please provide two number to divide.");
+
+        System.out.println("Enter first number: ");
+        double number1 = input.nextDouble();
+
+        System.out.println("Enter second number: ");
+        double number2 = input.nextDouble();
+
+        System.out.println("The quotient is " + divide(number1, number2));
+
+
+        input.close();
+    }
+
+    public static double divide(double nominator, double dominator) {
+
+        if (dominator == 0) {
+            System.err.println("Can not divide by 0");
+            System.exit(1);
+        }
+
+        double sign = 1;
+        if (nominator * dominator < 0) {
+            sign = -1;
+        }
+
+        nominator = Math.abs(nominator);
+        dominator = Math.abs(dominator);
+
+        double quotient = 0;
+
+        while (nominator >= dominator) {
+            nominator = nominator - dominator;
+            quotient++;
+        }
+
+        System.out.println("The remainder is " + nominator);
+
+        return sign * quotient;
+
+    }
+    }
+
