334f1310552b370da603b3b13a1573cfbf181dde
nadiiaurban <nadiiaurban@gmail.com>
2023-06-07 19:52:43 -0600

    added week2  tasks

---
+++ src/NadiiaUrban/week3/PrimeNumber.java
@@ -0,0 +1,18 @@
+package NadiiaUrban.week3;
+
+public class PrimeNumber {
+
+
+
+    public static boolean primeNumber(int num){
+       // for (int i = ; i < ; i++) {
+
+      //  }
+        return  true;
+    }
+}
+/*
+1. Numbers -- Prime Number
+Write a method that can check if a number is
+prime or not
+ */
