56d1f661c497fdcc9f5fac7ea19184086f7176e9
YuliiaOstgit config --global user.name YuliiaOstgit config --global user.name YuliiaOst <yuliya.ostrogrud@gmail.com>
2023-06-15 01:00:58 -0500

    Add RemoveDuplicates

---
+++ src/YuliiaOst/week04/RemoveDuplicates.java
@@ -0,0 +1,47 @@
+package YuliiaOst.week04;
+
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static String removeDup(String str) {
+
+        if (str.isEmpty()) {
+            System.out.println(("String is empty"));
+        } else if (str.isBlank()) {
+            System.out.println("String contains only empty spaces");
+        }
+
+        String result = "";
+
+        for (int i = 0; i <= str.length() - 1; i++) {
+            char letter = str.charAt(i);
+
+            if (result.contains(letter + "")) {
+                continue;
+            } else {
+                result += letter;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter any string: ");
+        String str = input.nextLine();
+
+        System.out.println(removeDup(str));
+
+        input.close();
+
+    }
+
+}
+
+/*
+Write a return method that can remove the duplicated values from String.
+Ex: removeDup("AAABBBCCC") ==> ABC
+ */
