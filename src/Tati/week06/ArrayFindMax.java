83d70957c03f7683e725af4ef10feeea8e2732d1
TatianaRasskazova <trasskazova840@gmail.com>
2023-07-12 00:13:01 -0500

    added week6 task2

--- src/Tati/week06/ArrayFindMax.java
+++ src/Tati/week06/ArrayFindMax.java
@@ -5,4 +5,19 @@ public class ArrayFindMax {
     2. Find Maximum:
   Write a method that can find the maximum number from an int Array.
      */
+    public static int arrayFindMax(int[] arr) {
+        int max = arr[0];
+        for (int each : arr) {
+            if(each>max)
+            {
+                max = each;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, 2, 3, 4, -89, 6, 0};
+        System.out.println(arrayFindMax(arr));
+    }
 }
